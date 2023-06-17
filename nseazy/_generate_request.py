b96bc5e606a67c61b1ffba5c50799d778a45be06
Dr. Chandrakant Bangar <127198654+DrChandrakant@users.noreply.github.com>
2023-06-17 11:53:46 +0530

    Updaate

---
+++ nseazy/_generate_request.py
@@ -0,0 +1,10 @@
+from nseazy._helpers import _headers, _cookies
+import requests
+
+method = True
+
+if(method is True):
+    def _fetch_data(api_url):
+        session = requests.Session()
+        rawData = session.get(api_url,headers=_headers,cookies=_cookies).json()
+        return rawData
--- nseazy/_instruments.py
+++ nseazy/_instruments.py
@@ -1,3 +1,20 @@
+
+from nseazy._generate_request import _fetch_data
+
 def _validate_symbol(symbol):
     symbol = symbol.replace('&','%26')
     return symbol
+
+
+def _validate_derivative():
+    positions = _fetch_data('https://www.nseindia.com/api/equity-stockIndices?index=SECURITIES%20IN%20F%26O')
+
+    nselist=['NIFTY','NIFTYIT','BANKNIFTY']
+
+    i=0
+    for x in range(i, len(positions['data'])):
+        nselist=nselist+[positions['data'][x]['symbol']]
+
+    return nselist
+
+fnolist = _validate_derivative()

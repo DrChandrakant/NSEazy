0854e14c8d0f53594c6dd22a5d290eddb64291f6
Dr. Chandrakant Bangar <127198654+DrChandrakant@users.noreply.github.com>
2023-06-17 13:38:44 +0530

    ok

--- nseazy/__init__.py
+++ nseazy/__init__.py
@@ -1,5 +1,5 @@
 from nseazy._version    import __version__
 from nseazy._kwarg_help import kwarg_help
 from nseazy._helpers import _cookies
-from nseazy._instruments import fnolist
+#from nseazy._instruments import fnolist
 from nseazy.fetch import *

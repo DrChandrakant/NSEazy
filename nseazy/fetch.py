6ff042600e33bc5289556985fc49cdc60ba74818
Dr. Chandrakant Bangar <127198654+DrChandrakant@users.noreply.github.com>
2023-06-17 13:47:09 +0530

    option Chain

--- nseazy/fetch.py
+++ nseazy/fetch.py
@@ -1,6 +1,6 @@
 import pandas as pd
 from nseazy._helpers import _base_api_url, _equity_quote_api, _derivative_quote_api
-from nseazy._instruments import _validate_symbol
+from nseazy._instruments import _validate_symbol, fnolist
 from nseazy._generate_request import _fetch_data
 
 
@@ -17,3 +17,13 @@ def nse_quote(symbol,section=""):
     if(section!=""):
         rawJson = _fetch_data( _base_api_url + _equity_quote_api + symbol+'&section='+section)
         return rawJson
+    
+
+def nse_optionchain_scrapper(symbol):
+    symbol, isDerivative = _validate_symbol(symbol)
+    if isDerivative is True:
+        rawOptionchain = _fetch_data( _base_api_url + _derivative_quote_api + symbol)
+    else:
+        rawptionchain = None
+        raise ValueError("Specified Symbol May Not Be List Under Derivative Segment Available List of Symbol", fnolist)
+    return rawptionchain

a77d9497dc1d66aa413567c4ee8fb2089ef0de52
Dr. Chandrakant Bangar <127198654+DrChandrakant@users.noreply.github.com>
2023-06-18 13:27:08 +0530

    Validation Method Updated
    Validation Method Updated

--- nseazy/_validators.py
+++ nseazy/_validators.py
@@ -1,13 +1,8 @@
 
 def _validate_vkwargs_dict(vkwargs):
-    """
-    Check that we didn't make a typo in any of the things
-    that should be the same for all vkwargs dict items:
 
-    {kwarg : {'Default': ... , 'Description': ... , 'Validator': ...} }
-    """
     for key, value in vkwargs.items():
-        # has been changed to 3 to support descriptions
+        
         if len(value) != 3:
             raise ValueError('Items != 3 in valid kwarg table, for kwarg "'+key+'"')
         if 'Default' not in value:
@@ -18,20 +13,11 @@ def _validate_vkwargs_dict(vkwargs):
             raise ValueError('Missing "Validator" function for kwarg "'+key+'"')
         
 def _check_kwargs(kwargs, vkwargs):
-    '''
-    Given a "valid kwargs table" and some kwargs, verify that each key-word
-    is valid per the kwargs table, and that the value of the kwarg is the
-    correct type.  Fill a configuration dictionary with the default value
-    for each kwarg, and then substitute in any values that were provided 
-    as kwargs and return the configuration dictionary.
-    '''
-    # initialize configuration from valid_kwargs_table:
+
     config  = {}
     for key, value in vkwargs.items():
         config[key] = value['Default']
 
-    # now validate kwargs, and for any valid kwargs
-    #  replace the appropriate value in config:
     for key in kwargs.keys():
         if key not in vkwargs:
             raise KeyError('Unrecognized kwarg="'+str(key)+'"')
@@ -47,10 +33,6 @@ def _check_kwargs(kwargs, vkwargs):
                 v = inspect.getsource(vkwargs[key]['Validator']).strip()
                 raise TypeError('kwarg "'+key+'" validator returned False for value: "'+str(value)+'"\n    '+v)
 
-       # ---------------------------------------------------------------
-       #  At this point in the loop, if we have not raised an exception,
-       #      then kwarg is valid as far as we can tell, therefore, 
-       #      go ahead and replace the appropriate value in config:
 
         config[key] = value
 

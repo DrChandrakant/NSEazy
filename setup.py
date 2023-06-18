e2dd60dec4bf9972d79d26dc8047ce7ac234daef
Dr. Chandrakant Bangar <127198654+DrChandrakant@users.noreply.github.com>
2023-06-18 22:03:47 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import find_packages
 pkg_location = ''
 pkg_name     = 'nseasy'
 
-vfile = vfile = 'nseazy/_version.py'
+vfile = vfile = '/_version.py'
 vers = {}
 with open(vfile) as f:
    exec(f.read(), {}, vers)

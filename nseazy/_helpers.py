172f26a120e8d21e2964dcc87cb6c74ab9e4ade4
Dr. Chandrakant Bangar <127198654+DrChandrakant@users.noreply.github.com>
2023-06-18 13:28:44 +0530

    API Updated
    API Updated

--- nseazy/_helpers.py
+++ nseazy/_helpers.py
@@ -28,8 +28,6 @@ def get_cookies(_base_api_url):
     return cookies
 
 
-_curl_headers = ''' -H "authority: beta.nseindia.com" -H "cache-control: max-age=0" -H "dnt: 1" -H "upgrade-insecure-requests: 1" -H "user-agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.117 Safari/537.36" -H "sec-fetch-user: ?1" -H "accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9" -H "sec-fetch-site: none" -H "sec-fetch-mode: navigate" -H "accept-encoding: gzip, deflate, br" -H "accept-language: en-US,en;q=0.9,hi;q=0.8" --compressed'''
-
 _base_api_url = "https://www.nseindia.com/"
 
 _equity_quote_api = "api/quote-equity?symbol="

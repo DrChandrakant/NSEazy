caaf83648f3ef2315b2dde6bf3497201b99602f2
Dr. Chandrakant Bangar <127198654+DrChandrakant@users.noreply.github.com>
2023-06-20 16:55:57 +0530

    Update Checks

--- nseazy/__init__.py
+++ nseazy/__init__.py
@@ -1,5 +1,5 @@
 import nseazy._warnings
-from nseazy._version    import __version__
+from nseazy._version import __version__
 from nseazy._help import help
 from nseazy._helpers import _cookies
 from nseazy._instruments import fnolist
